f0c768507238b255ceff7bccfb3b76ad772f0978
Milos <milosrancic94@gmail.com>
2019-08-04 21:54:49 +0200

    started editing contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,45 @@ import "../styles/Contact.css";
 const Contact = () => {
   return (
     <div id="contact" className="container">
-      {/* <p className="text-left">Contact</p> */}
-      <p className="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo laborum
-        qui, velit, cumque, ea numquam possimus quos saepe corrupti consectetur
-        quo dolor sint maiores? Voluptates, doloribus? Cum, alias? Accusantium
-        consequatur ab placeat odio quis doloribus?
-      </p>
+      {/* <p classNameName="text-left">Contact</p> */}
+      <h3>Contact us</h3>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Full Name</label>
+          <input
+            type="text"
+            className="form-control"
+            id="fullName"
+            aria-describedby="fullName"
+            placeholder="Enter your name"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            We'll never share your email with anyone else.
+          </small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
