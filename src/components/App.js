1deaedb17bc3edff8bef1214062cf8879b32b3da
Milos <milosrancic94@gmail.com>
2019-08-04 20:45:24 +0200

    added react router into code

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "../styles/App.css";
 import A from "./A.js";
 import B from "./B.js";
@@ -7,11 +8,37 @@ import C from "./C.js";
 class App extends React.Component {
   render() {
     return (
-      <div className="App text-center">
-        <A />
-        <B />
-        <C />
-      </div>
+      <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className="navbar-nav mr-auto">
+              <li>
+                <Link to={"/"} className="nav-link">
+                  {" "}
+                  A component
+                </Link>
+              </li>
+              <li>
+                <Link to={"/b"} className="nav-link">
+                  B component
+                </Link>
+              </li>
+              <li>
+                <Link to={"/c"} className="nav-link">
+                  C component
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <hr />
+          <Switch>
+            <Route exact path="/" component={A} />
+            <Route path="/b" component={B} />
+            <Route path="/c" component={C} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
