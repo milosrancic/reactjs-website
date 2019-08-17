2683c1af374ddecc498f376e9d006f3f476a83c1
Milos <milosrancic94@gmail.com>
2019-08-18 00:05:58 +0200

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,17 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <div className="container fixed-bottom" id="footer">
-      <p className="container float-left">
-        Newsletter: _____{" "}
-        <button type="button" class="btn btn-sm btn-outline-dark">
-          Submit
-        </button>
-        <p className="float-right">by: Milos Rancic</p>
-      </p>
+      <div className="container float-left">
+        <small>
+          Newsletter: _________{" "}
+          <button type="button" className="btn btn-sm btn-outline-dark">
+            Submit
+          </button>
+        </small>
+        <p className="float-right">
+          <small>by: Milos Rancic</small>
+        </p>
+      </div>
     </div>
   );
 };
