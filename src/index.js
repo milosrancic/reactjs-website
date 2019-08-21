5cf440de7e31417912d9ea5c9e82408dbd1e6cca
Milos <milosrancic94@gmail.com>
2019-08-21 19:15:47 +0200

    installed react calendar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import Calendar from "react-calendar";
 import "./index.css";
 import App from "./components/App";
 
