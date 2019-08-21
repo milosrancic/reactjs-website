932154984015a298f210b01f27216966b151c037
Milos <milosrancic94@gmail.com>
2019-08-21 19:17:00 +0200

    added calendar to bookings component

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/Bookings.css";
+import Calendar from "react-calendar";
 
 const Bookings = () => {
   return (
     <div id="bookings" className="container">
       <p>Bookings component</p>
+      <div className="row">
+        <Calendar />
+        <Calendar />
+      </div>
     </div>
   );
 };
