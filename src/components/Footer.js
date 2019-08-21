c2176ff27018ffe5ca4eb2713265c5592b3fc80c
Milos <milosrancic94@gmail.com>
2019-08-21 19:08:12 +0200

    edited newsletter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,13 @@ const Footer = () => {
   return (
     <div className="container fixed-bottom" id="footer">
       <div className="container float-left">
-        <small>
-          Newsletter: _________{" "}
-          <button type="button" className="btn btn-sm btn-outline-dark">
-            Submit
-          </button>
+        <small className="text-newsletter">
+          Newsletter: <input type="text" className="input-newsletter" />
         </small>
+        <button type="button" className="ml-1 mt-0 btn btn-sm btn-outline-dark">
+          Submit
+        </button>
+
         <p className="float-right">
           <small>by: Milos Rancic</small>
         </p>
