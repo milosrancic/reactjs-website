2146a98150ad1260a09ba303af5bfab9c1e9d751
Milos <milosrancic94@gmail.com>
2019-08-23 21:55:22 +0200

    changer place of footer component in react router

--- src/components/App.js
+++ src/components/App.js
@@ -12,38 +12,40 @@ import Footer from "./Footer";
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/about"} className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/bookings"} className="nav-link">
-              Bookings
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/contact"} className="nav-link">
-              Contact
-            </Link>
-          </li>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/bookings" component={Bookings} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NotFound} />
-          </Switch>
-        </ul>
+      <div>
+        <Router>
+          <ul className="nav justify-content-center">
+            <li className="nav-item">
+              <Link to={"/"} className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/about"} className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/bookings"} className="nav-link">
+                Bookings
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/contact"} className="nav-link">
+                Contact
+              </Link>
+            </li>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/bookings" component={Bookings} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NotFound} />
+            </Switch>
+          </ul>
+        </Router>
         <Footer />
-      </Router>
+      </div>
     );
   }
 }
