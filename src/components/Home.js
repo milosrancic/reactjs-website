10b3de4d863e091b8126813354730d3392e042e6
Milos <milosrancic94@gmail.com>
2019-08-23 22:10:43 +0200

    new possible layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,83 +17,70 @@ const Home = () => {
 
       <div className="row">
         <div className="col">
-          <div class="card bg-dark text-white">
+          <div className="card bg-dark text-white">
             <img
               src={
                 "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAATUAAACjCAMAAADciXncAAAAA1BMVEX/ZgAqRcUWAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBrxWIAAQqkhDkAAAAASUVORK5CYII="
               }
-              class="card-img img-fluid"
+              className="card-img img-fluid"
               alt="..."
             />
-            <div class="card-img-overlay ">
-              <h5 class="card-title align-middle">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+            <div className="card-img-overlay ">
+              <h5 className="card-title text-center align-middle">
+                ACCOMODATION
+              </h5>
             </div>
           </div>
         </div>
 
         <div className="col">
-          <div class="card bg-dark text-white">
+          <div className="card bg-dark text-white">
             <img
               src={
                 "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAATUAAACjCAMAAADciXncAAAAA1BMVEX/ZgAqRcUWAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBrxWIAAQqkhDkAAAAASUVORK5CYII="
               }
-              class="card-img img-fluid"
+              className="card-img img-fluid"
               alt="..."
             />
-            <div class="card-img-overlay">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+            <div className="card-img-overlay">
+              <h5 className="card-title">WEDDINGS</h5>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="row">
+      <div className="row mt-3">
         <div className="col">
-          <div class="card bg-dark text-white">
+          <div className="card bg-dark text-white">
             <img
               src={
                 "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAATUAAACjCAMAAADciXncAAAAA1BMVEX/ZgAqRcUWAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBrxWIAAQqkhDkAAAAASUVORK5CYII="
               }
-              class="card-img img-fluid"
+              className="card-img img-fluid"
               alt="..."
             />
-            <div class="card-img-overlay ">
-              <h5 class="card-title align-middle">Card title</h5>
-              <p class="card-text">
+            <div className="card-img-overlay ">
+              <h5 className="card-title align-middle">Card title</h5>
+              {/* <p className="card-text">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
 
         <div className="col">
-          <div class="card bg-dark text-white">
+          <div className="card bg-dark text-white">
             <img
               src={
                 "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAATUAAACjCAMAAADciXncAAAAA1BMVEX/ZgAqRcUWAAAAR0lEQVR4nO3BAQ0AAADCoPdPbQ8HFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPBrxWIAAQqkhDkAAAAASUVORK5CYII="
               }
-              class="card-img img-fluid"
+              className="card-img img-fluid"
               alt="..."
             />
-            <div class="card-img-overlay">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+            <div className="card-img-overlay">
+              <h5 className="card-title">Card title</h5>
             </div>
           </div>
         </div>
