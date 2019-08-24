9152d05e88479a5b9e8cdfdecbc113975e2e89b7
Milos <milosrancic94@gmail.com>
2019-08-24 22:44:15 +0200

    footer needs to be deleted, for now everything is commented out

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,23 @@
-import React from "react";
-import "../styles/Footer.css";
+// import React from "react";
+// import "../styles/Footer.css";
 
-const Footer = () => {
-  return (
-    <div className="container fixed-bottom " id="footer">
-      <div className="float-left">
-        <small className="text-newsletter">
-          Newsletter: <input type="text" className="input-newsletter" />
-        </small>
-        <button type="button" className="ml-1 mt-0 btn btn-sm btn-outline-dark">
-          Submit
-        </button>
-      </div>
+// const Footer = () => {
+//   return (
+//     <div className="container fixed-bottom " id="footer">
+//       <div className="float-right">
+//         <small className="author-text">
+//           by: Milos Rancic{" "}
+//           <a
+//             href="https://github.com/milosrancic"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             <i className="fab fa-github" />
+//           </a>
+//         </small>
+//       </div>
+//     </div>
+//   );
+// };
 
-      <div className="float-right">
-        <small className="author-text">
-          by: Milos Rancic{" "}
-          <a
-            href="https://github.com/milosrancic"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-github" />
-          </a>
-        </small>
-      </div>
-    </div>
-  );
-};
-
-export default Footer;
+// export default Footer;
