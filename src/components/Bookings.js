92dc2c538456f5951ef544823c959205e554f031
Milos <milosrancic94@gmail.com>
2019-09-04 19:19:08 +0200

    /

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -2,24 +2,6 @@ import React from "react";
 import "../styles/Bookings.css";
 import Calendar from "react-calendar";
 
-import ReactCalendar from "react-calendar/dist/entry.nostyle";
-
-// helpers
-// const dateAlreadyClicked = (dates, date) => dates.some(d => dayjs(date).isSame(dayjs(d), 'day'))
-// const datesExcept = (dates, date) => dates.filter(d => !dayjs(date).isSame(dayjs(d), 'day'))
-
-const Bookings = () => {
-  // const [dates, setDates] = useState([])
-
-  const onClickDay = date => {};
-
-  const tileClassName = ({ date }) => {};
-
-  return (
-    <ReactCalendar tileClassName={tileClassName} onClickDay={onClickDay} />
-  );
-};
-
 // const Bookings = () => {
 //   return (
 //     <div id="bookings" className="container">
@@ -38,39 +20,43 @@ const Bookings = () => {
 //   );
 // };
 
-// class Bookings extends React.Component {
-//   state = {
-//     date1: new Date(),
-//     date2: new Date()
-//   };
+class Bookings extends React.Component {
+  state = {
+    date1: new Date(),
+    date2: new Date()
+  };
 
-//   onChange1 = date1 => this.setState({ date1 }, console.log(date1));
-//   onChange2 = date2 => this.setState({ date2 }, console.log(date2));
+  onChange1 = date1 => this.setState({ date1 }, console.log("DATE 1: ", date1));
+  onChange2 = date2 => this.setState({ date2 }, console.log("DATE 2: ", date2));
 
-//   daysTotal = () => {
-//     console.log(this.state.date1, this.state.date2);
-//   };
+  daysTotal = () => {
+    // console.log(this.state.date1, this.state.date2);
+  };
 
-//   render() {
-//     // let dateA = this.state.date1;
-//     // let date2 = this.state.date2;
+  onConsole = () => {
+    console.log(this.state.date1.split(" "));
+  };
 
-//     return (
-//       <div id="bookings" className="container">
-//         <div className="row">
-//           <div className="col">
-//             <p>Check-in</p>
-//             <Calendar onChange={this.onChange1} value={this.state.date1} />
-//           </div>
-//           <div className="col">
-//             <p>Check-out</p>
-//             <Calendar onChange={this.onChange2} value={this.state.date2} />
-//           </div>
-//         </div>
-//         <p className="text-center">{this.daysTotal()}</p>
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    // let dateA = this.state.date1;
+    // let date2 = this.state.date2;
+
+    return (
+      <div id="bookings" className="container">
+        <div className="row">
+          <div className="col">
+            <p>Check-in</p>
+            <Calendar onChange={this.onChange1} value={this.state.date1} />
+          </div>
+          <div className="col">
+            <p>Check-out</p>
+            <Calendar onChange={this.onChange2} value={this.state.date2} />
+          </div>
+        </div>
+        <p className="text-center">{this.daysTotal()}</p>
+      </div>
+    );
+  }
+}
 
 export default Bookings;
