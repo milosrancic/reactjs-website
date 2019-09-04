3ab6355e47afb2f1ceaabe92b04864df731c90ce
Milos <milosrancic94@gmail.com>
2019-09-04 18:38:14 +0200

    added placeholder code

---
+++ src/components/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Gallery.css";
+
+const Gallery = () => {
+  return (
+    <div className="container">
+      <h3>gallery component</h3>
+    </div>
+  );
+};
+export default Gallery;
