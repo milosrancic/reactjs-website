70101956b8e13a38ac14d7652e6b8a2e78650915
Milos <milosrancic94@gmail.com>
2019-12-03 22:51:54 +0100

    made footer stick to bottom of page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,27 +19,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-// import React from "react";
-// import "../styles/Footer.css";
-
-// const Footer = () => {
-//   return (
-//     <div className="container fixed-bottom " id="footer">
-//       <div className="float-right">
-//         <small className="author-text">
-//           by: Milos Rancic{" "}
-//           <a
-//             href="https://github.com/milosrancic"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <i className="fab fa-github" />
-//           </a>
-//         </small>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Footer;
