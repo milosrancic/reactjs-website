f2ec21ca706976983196e6f0fcb26581f67884eb
Milos <milosrancic94@gmail.com>
2019-12-09 19:41:28 +0100

    started with new component layout

--- src/components/Accomodation.js
+++ src/components/Accomodation.js
@@ -1,77 +1,91 @@
 import React from "react";
 import "../styles/Accomodation.css";
 
+import accomodation from "../images/accomodation.jpg";
+
 const Accomodation = () => {
   return (
     <div id="accomodation">
-      {/* <p className="text-left">About</p> */}
       <h3>Accomodation</h3>
-      <p className="text-justify">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque odio
-        eius ducimus tenetur esse a! Aspernatur ipsam sapiente modi magnam
-        impedit, id blanditiis debitis quis, quibusdam voluptatem necessitatibus
-        sit eius. Facilis magnam ipsam blanditiis harum? Est totam eum eveniet
-        consectetur earum, alias in numquam similique ratione aperiam esse
-        aspernatur. Laborum, quaerat animi aliquam quis dignissimos quod aperiam
-        aut sapiente cumque, ipsam tenetur placeat eaque facere repellat
-        provident, voluptatibus eum officiis? Dolores quidem maiores corporis
-        corrupti voluptatum molestiae, dolorem ratione incidunt ullam placeat
-        suscipit totam id, atque nihil deleniti necessitatibus excepturi? Nihil
-        distinctio non sed mollitia culpa cumque odio voluptate natus.
-        Temporibus numquam ipsa neque amet! Eius odit in inventore sequi
-        adipisci? Nisi praesentium excepturi esse quibusdam, voluptates
-        provident est neque rem. Ipsum inventore totam eaque nihil adipisci est
-        debitis soluta quae expedita velit veniam ut reprehenderit deserunt
-        accusamus hic odit asperiores, ullam dicta, odio ex neque in?
-        Perferendis, dolor cumque.
-      </p>
+      <div className="row">
+        <div className="col">
+          <img src={accomodation} className="img-fluid" alt="" />
+        </div>
+        <div className="col border">
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consequatur temporibus doloremque distinctio pariatur veritatis.
+            Consequuntur qui omnis minus ad tenetur aspernatur corrupti
+            voluptatum quia et asperiores accusamus suscipit exercitationem vero
+            voluptates aliquid, odit quisquam ipsam nostrum provident? Natus hic
+            rerum at beatae dolorem ut qui officia asperiores. Quas, molestiae?
+          </p>
+        </div>
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quos ipsam
-        quisquam facilis voluptatibus molestiae harum, sint placeat,
-        repudiandae, doloremque nihil porro assumenda. Voluptatum, nulla nisi.
-        Voluptate animi, dolore corrupti ipsam aut possimus natus! Sapiente
-        nostrum nisi doloremque a quasi excepturi cumque saepe iure iusto
-        accusamus temporibus nulla omnis ipsam optio aspernatur molestias porro
-        odit commodi, debitis quidem? Sit culpa modi aliquam blanditiis a ad
-        ullam veniam odit distinctio in quidem corrupti, veritatis dolorem,
-        expedita natus eaque ipsa explicabo sequi laborum accusantium!
-        Asperiores consequuntur repellat et rem, rerum molestias ipsam,
-        laboriosam natus animi expedita ratione aperiam! Quas, incidunt? In,
-        ipsum aperiam iusto natus nihil quaerat rerum accusantium. Magni maxime
-        cupiditate enim porro eos minus voluptas omnis nobis quisquam labore
-        sequi possimus fugit, unde inventore tenetur voluptate eligendi debitis,
-        ratione officiis odit laborum aspernatur? Quidem, quia. Maxime voluptas
-        expedita nesciunt, incidunt quisquam porro sit, architecto veritatis
-        deserunt, quaerat culpa voluptatibus qui. Unde nobis beatae dicta.
-        Excepturi rerum dicta optio consectetur in possimus, necessitatibus
-        ipsum, architecto quia, magni assumenda blanditiis iure voluptates sint.
-        Nisi magnam minima reprehenderit porro earum in perferendis sint rerum
-        molestias quia. Pariatur, asperiores magni, consequatur quasi, eius at
-        tempore exercitationem reprehenderit dolore magnam iusto nobis ad
-        distinctio placeat suscipit officia voluptas ipsa! Quod nisi distinctio
-        dicta soluta, quibusdam quis, reprehenderit suscipit facere, inventore
-        minus culpa sit ipsam cum iste aspernatur iure. Atque, dicta. Rem, eum
-        quidem vel eveniet quisquam aperiam libero quam tenetur illo cum
-        excepturi, culpa quaerat minus, quia ducimus? Delectus hic nobis, iure
-        atque, accusamus provident sequi obcaecati deleniti et repudiandae optio
-        voluptas ab molestias. Harum unde voluptatum facilis magnam sit laborum,
-        explicabo, quod atque minus reiciendis molestiae, aliquam animi a
-        inventore veniam tenetur facere dolore quisquam voluptates doloribus vel
-        fugit nihil reprehenderit soluta? Quos, explicabo doloremque soluta nemo
-        iure blanditiis minus modi laboriosam quam alias voluptate ratione ab
-        nisi quo quae odio maiores repellat tenetur. Deserunt hic in cumque vel
-        ea accusantium quisquam expedita fugiat ratione, tempora quaerat odit
-        mollitia, iure doloribus blanditiis nihil. Dolorem numquam voluptatem
-        perferendis earum incidunt deleniti labore cupiditate fugiat iusto,
-        fugit aspernatur eligendi dolores laudantium facere vitae reiciendis. Ad
-        delectus, autem id excepturi alias ratione placeat quia in numquam
-        eligendi accusamus totam ut saepe ipsa assumenda illum dignissimos
-        molestias voluptatibus. Magnam illo, aliquid quia quo molestias omnis
-        pariatur? Blanditiis ratione dolor similique hic id laborum fugiat
-        debitis accusamus. Sapiente nobis consectetur illo cumque reprehenderit
-        dolore cupiditate iusto in beatae ad.
-      </p>
+      <div className="row mt-5 border">
+        <div className="col">
+          <img src={accomodation} className="img-fluid" alt="" />
+        </div>
+        <div className="col ">
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consequatur temporibus doloremque distinctio pariatur veritatis.
+            Consequuntur qui omnis minus ad tenetur aspernatur corrupti
+            voluptatum quia et asperiores accusamus suscipit exercitationem vero
+            voluptates aliquid, odit quisquam ipsam nostrum provident? Natus hic
+            rerum at beatae dolorem ut qui officia asperiores. Quas, molestiae?
+          </p>
+        </div>
+      </div>
+
+      <div className="row mt-5 border">
+        <div className="col">
+          <img src={accomodation} className="img-fluid" alt="" />
+        </div>
+        <div className="col ">
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consequatur temporibus doloremque distinctio pariatur veritatis.
+            Consequuntur qui omnis minus ad tenetur aspernatur corrupti
+            voluptatum quia et asperiores accusamus suscipit exercitationem vero
+            voluptates aliquid, odit quisquam ipsam nostrum provident? Natus hic
+            rerum at beatae dolorem ut qui officia asperiores. Quas, molestiae?
+          </p>
+        </div>
+      </div>
+
+      <div className="row mt-5 border">
+        <div className="col">
+          <img src={accomodation} className="img-fluid" alt="" />
+        </div>
+        <div className="col ">
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consequatur temporibus doloremque distinctio pariatur veritatis.
+            Consequuntur qui omnis minus ad tenetur aspernatur corrupti
+            voluptatum quia et asperiores accusamus suscipit exercitationem vero
+            voluptates aliquid, odit quisquam ipsam nostrum provident? Natus hic
+            rerum at beatae dolorem ut qui officia asperiores. Quas, molestiae?
+          </p>
+        </div>
+      </div>
+
+      <div className="row mt-5 border">
+        <div className="col">
+          <img src={accomodation} className="img-fluid" alt="" />
+        </div>
+        <div className="col ">
+          <p className="">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
+            consequatur temporibus doloremque distinctio pariatur veritatis.
+            Consequuntur qui omnis minus ad tenetur aspernatur corrupti
+            voluptatum quia et asperiores accusamus suscipit exercitationem vero
+            voluptates aliquid, odit quisquam ipsam nostrum provident? Natus hic
+            rerum at beatae dolorem ut qui officia asperiores. Quas, molestiae?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
