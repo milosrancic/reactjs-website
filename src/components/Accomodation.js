f35e2db6a5ec9e3fb921f2502e8c0bb255853121
Milos <milosrancic94@gmail.com>
2019-12-12 16:07:05 +0100

    code cleanup and added new props

--- src/components/Accomodation.js
+++ src/components/Accomodation.js
@@ -64,6 +64,7 @@ const Accomodation = () => {
           beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="1 Queen Bed & 2 Single Beds"
         people="4"
+        feedback="Superb"
         img={accomodation3}
       />
       <Room
@@ -77,85 +78,9 @@ const Accomodation = () => {
           beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="2 Queen Beds & 2 Single Beds"
         people="6"
+        feedback="Guest favorite"
         img={accomodation1}
       />
-      {/* <div className="row mt-5">
-        <div className="col-md-8">
-          <img src={accomodation2} className="img-fluid" alt="" />
-        </div>
-        <div className="col-md-4">
-          <h4>Standard Double Room</h4>
-          <p>
-            <i class="fas fa-bed"> 1 Twin bed</i>
-          </p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?
-        </div>
-      </div>
-
-      <div className="row mt-5">
-        <div className="col-md-8">
-          <img src={accomodation5} className="img-fluid" alt="" />
-        </div>
-        <div className="col-md-4">
-          <h4>Standard Double Room</h4>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?
-        </div>
-      </div>
-
-      <div className="row mt-5">
-        <div className="col-md-8">
-          <img src={accomodation4} className="img-fluid" alt="" />
-        </div>
-        <div className="col-md-4">
-          <h4>Standard Suite</h4>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?
-        </div>
-      </div>
-
-      <div className="row mt-5">
-        <div className="col-md-8">
-          <img src={accomodation3} className="img-fluid" alt="" />
-        </div>
-        <div className="col-md-4">
-          <h4>Standard Suite</h4>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?
-        </div>
-      </div>
-
-      <div className="row mt-5">
-        <div className="col-md-8">
-          <img src={accomodation1} className="img-fluid" alt="" />
-        </div>
-        <div className="col-md-4">
-          <h4>Family Suite</h4>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?
-        </div>
-      </div> */}
     </div>
   );
 };
