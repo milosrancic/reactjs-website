6315a8bc29d5516d5925770bce3b76b7c88f618d
Milos <milosrancic94@gmail.com>
2019-12-14 16:31:46 +0100

    code cleanup

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // import MoreInfo from "./MoreInfo";
-import "../styles/Room.css";
+import "../styles/Room.scss";
 
 // each room will have its own "more information" button, and when hovered over it will show more info right there
 
@@ -17,15 +17,9 @@ const Room = props => {
           <img src={props.img} className="img-fluid" alt="" />
         </div>
         <div className="col-md-4">
-          <span>{props.feedback}</span>
+          <span className="feedback">{props.feedback}</span>
           <h3>{props.title}</h3>
           <p>{props.subtitle}</p>
-          {/* <span className="room-details">
-            <i className="fas fa-bed"> {props.beds}</i>
-          </span>
-          <span className="room-details">
-            <i className="fas fa-male"> x {props.people}</i>
-          </span> */}
           <ul>
             <li>
               <i className="fas fa-bed"></i> {props.beds}
@@ -34,13 +28,20 @@ const Room = props => {
               <i className="fas fa-male"></i> x {props.people}
             </li>
             <li>
-              <i className="fas fa-utensils"></i> {props.complimentary}
+              <i className="fas fa-glass-martini-alt"></i>
+              {props.bar}
+            </li>
+            <li>
+              <i className="fas fa-utensils"></i> {props.food}
+            </li>
+            <li>
+              <i className="fas fa-dumbbell"></i>
+              {props.gym}
             </li>
             <li>
               <i className="fas fa-spa"></i> {props.spa}
             </li>
           </ul>
-
           {/* <button className="btn btn-outline-secondary">More Info</button> */}
         </div>
       </div>
