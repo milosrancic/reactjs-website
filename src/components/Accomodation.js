27865dafb64b4affc78c7610f7d4153c25159432
Milos <milosrancic94@gmail.com>
2019-12-14 15:24:05 +0100

    finished facility icons layout

--- src/components/Accomodation.js
+++ src/components/Accomodation.js
@@ -17,77 +17,71 @@ const Accomodation = () => {
       <Room
         title="Standard Double Room"
         subtitle="Start from $70/night"
-        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="1 Queen Bed"
         people="2"
+        complimentary="Free breakfast"
+        spa="30% discount on spa"
         img={accomodation2}
       />
       <Room
         title="Standard Double Room"
         subtitle="Start from $70/night"
-        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae?"
         beds="1 Queen Bed"
         people="2"
+        complimentary="Free breakfast"
+        spa="30% discount on spa"
         img={accomodation5}
       />
       <Room
         title="Standard Suite"
         subtitle="Start from $120/night"
-        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="1 Queen Bed & 1 Single Bed"
         people="3"
+        complimentary="Free lunch"
+        spa="50% discount on spa"
         img={accomodation4}
       />
       <Room
         title="Standard Suite"
         subtitle="Start from $120/night"
-        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="1 Queen Bed & 2 Single Beds"
         people="4"
+        complimentary="Free lunch"
+        spa="50% discount on spa"
         feedback="Superb"
         img={accomodation3}
       />
       <Room
         title="Family Suite"
         subtitle="Start from $220/night"
-        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis
-          consequatur temporibus doloremque distinctio pariatur veritatis.
-          Consequuntur qui omnis minus ad tenetur aspernatur corrupti voluptatum
-          quia et asperiores accusamus suscipit exercitationem vero voluptates
-          aliquid, odit quisquam ipsam nostrum provident? Natus hic rerum at
-          beatae dolorem ut qui officia asperiores. Quas, molestiae? "
         beds="2 Queen Beds & 2 Single Beds"
         people="6"
+        complimentary="Free breakfast & lunch"
+        spa="Complimentary spa"
         feedback="Guest favorite"
         img={accomodation1}
       />
       <div className="text-center mt-5 mb-4">
-        <i class="fas fa-wifi"></i>Free WiFi
-        <i class="fas fa-parking"></i>Parking <i class="far fa-clock"></i>
-        24-hour front desk <br />
-        <i class="fas fa-wheelchair"></i>Facilities for disabled guests
-        <i class="fas fa-paw"></i>Pets allowed{" "}
-        <i class="fas fa-concierge-bell"></i>Daily housekeeping
+        <ul>
+          <li>
+            <i className="fas fa-wifi"></i>Free WiFi
+          </li>
+          <li>
+            <i className="fas fa-parking"></i>Parking
+          </li>
+          <li>
+            <i className="fas fa-clock"></i>24-hour front desk
+          </li>
+          <li>
+            <i className="fas fa-wheelchair"></i>Facilities for disabled guests
+          </li>
+          <li>
+            <i className="fas fa-paw"></i>Pets allowed
+          </li>
+          <li>
+            <i className="fas fa-concierge-bell"></i>Daily housekeeping
+          </li>
+        </ul>
       </div>
     </div>
   );
