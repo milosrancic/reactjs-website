405a187cab60eefe57b19dc0a0ccc95c86b67e10
Milos <milosrancic94@gmail.com>
2019-12-23 17:06:35 +0100

    moved navigation to separate component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import "../styles/App.css";
 
+import Navbar from "./Navbar";
 import Home from "./Home.js";
 import Accomodation from "./Accomodation.js";
 import Gallery from "./Gallery.js";
@@ -15,46 +16,15 @@ class App extends React.Component {
   render() {
     return (
       <div id="app" className="container-fluid p-0">
-        <Router>
-          {/* nav wiil be transparent */}
-          <nav>
-            <ul className="nav justify-content-center">
-              <li className="nav-item">
-                <Link to={"/"} className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/accomodation"} className="nav-link">
-                  Accomodation
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/gallery"} className="nav-link">
-                  Gallery
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/bookings"} className="nav-link">
-                  Bookings
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/contact"} className="nav-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/accomodation" component={Accomodation} />
-            <Route path="/gallery" component={Gallery} />
-            <Route path="/bookings" component={Bookings} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/accomodation" component={Accomodation} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/bookings" component={Bookings} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
         <Footer />
       </div>
     );
