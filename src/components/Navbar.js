aeb018f481a86c10a2e969fa13ccdf9731f0aa72
Milos <milosrancic94@gmail.com>
2019-12-23 17:40:26 +0100

    code cleanup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,37 +3,35 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div id="navbar" className="container-fluid">
-      <nav className="navbar-inverse">
-        <ul className="nav  justify-content-center">
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/accomodation"} className="nav-link">
-              Accomodation
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/gallery"} className="nav-link">
-              Gallery
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/bookings"} className="nav-link">
-              Bookings
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/contact"} className="nav-link">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav id="navbar" className="container-fluid">
+      <ul className="nav justify-content-center  ">
+        <li className="nav-item">
+          <Link to={"/"} className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/accomodation"} className="nav-link">
+            Accomodation
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/gallery"} className="nav-link">
+            Gallery
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/bookings"} className="nav-link">
+            Bookings
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to={"/contact"} className="nav-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 export default Navbar;
