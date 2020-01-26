7e3a2837961291d42c1cf889d6c2693177bef8cb
Milos <milosrancic94@gmail.com>
2020-01-26 18:50:28 +0100

    color editing

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import "../styles/Home.scss";
 
 // images
 import accomodation from "../images/accomodation-01.jpg";
-import weddings from "../images/weddings.jpg";
 
 const Home = () => {
   return (
@@ -18,46 +17,58 @@ const Home = () => {
         </div>
       </div>
 
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src={accomodation} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
+      <div className="card-group">
+        <div className="card">
+          <img
+            className="card-img-top"
+            src={accomodation}
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
               longer.
             </p>
           </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src={accomodation} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
+        <div className="card">
+          <img
+            className="card-img-top"
+            src={accomodation}
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
               This card has supporting text below as a natural lead-in to
               additional content.
             </p>
           </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
           </div>
         </div>
-        <div class="card">
-          <img class="card-img-top" src={accomodation} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
+        <div className="card">
+          <img
+            className="card-img-top"
+            src={accomodation}
+            alt="Card image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This card has even longer content
               than the first to show that equal height action.
             </p>
           </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
+          <div className="card-footer">
+            <small className="text-muted">Last updated 3 mins ago</small>
           </div>
         </div>
       </div>
