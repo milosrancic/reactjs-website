1160a47420c79cbc167d680850d34412543e6d2f
Milos <milosrancic94@gmail.com>
2020-03-10 17:08:56 +0100

    imported datepicker

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/Bookings.css";
-import Calendar from "react-calendar";
+// import Calendar from "react-calendar";
+import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
 // const Bookings = () => {
 //   return (
@@ -22,38 +23,35 @@ import Calendar from "react-calendar";
 
 class Bookings extends React.Component {
   state = {
-    date1: new Date(),
-    date2: new Date()
+    date: [new Date(), new Date()],
   };
 
-  onChange1 = date1 => this.setState({ date1 }, console.log("DATE 1: ", date1));
-  onChange2 = date2 => this.setState({ date2 }, console.log("DATE 2: ", date2));
 
   daysTotal = () => {
-    // console.log(this.state.date1, this.state.date2);
+    return
   };
 
-  onConsole = () => {
-    console.log(this.state.date1.split(" "));
-  };
+  onChange = date => this.setState({ date })
 
   render() {
     // let dateA = this.state.date1;
     // let date2 = this.state.date2;
 
     return (
-      <div id="bookings" className="container">
-        <div className="row">
-          <div className="col">
-            <p>Check-in</p>
-            <Calendar onChange={this.onChange1} value={this.state.date1} />
-          </div>
-          <div className="col">
-            <p>Check-out</p>
-            <Calendar onChange={this.onChange2} value={this.state.date2} />
-          </div>
-        </div>
-        <p className="text-center">{this.daysTotal()}</p>
+      <div id="bookings" className="container text-center">
+
+        <p>Check-in</p>
+        <DateRangePicker
+          onChange={this.onChange}
+          value={this.state.date}
+          isOpen={true}
+          format="dd-MM-y"
+          rangeDivider=" to "
+        />
+        {/* </div> */}
+
+
+        <p className="text-left">you are staying for {this.daysTotal()} days </p>
       </div>
     );
   }
