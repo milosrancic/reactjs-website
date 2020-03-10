a6bad560d6f055c53ded6cded89e98001a70c519
Milos <milosrancic94@gmail.com>
2020-03-10 16:22:16 +0100

    created new component  activity card

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "../../styles/Activities.css";
 import CardDeck from 'react-bootstrap/CardDeck'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import ActivityCard from "./ActivityCard";
 
-// images
+
 import activities01 from "../../images/activities-01.jpeg";
 import activities02 from "../../images/activities-02.jpeg";
 import activities03 from "../../images/activities-03.jpeg";
@@ -16,61 +15,33 @@ const Activities = () => {
       <h3>Things to do </h3>
 
       <CardDeck>
-        <Card>
-          <Card.Img variant="top" src={activities02} />
-          <Card.Body>
-            <Card.Title>Hiking</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-      </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={activities01} />
-          <Card.Body>
-            <Card.Title>Horseback riding</Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
+        <ActivityCard
+          img={activities02}
+          title="Hiking"
+          text="This is a wider card with supporting text below as a natural lead-in to
+              additional content. This content is a little bit longer."
+         />
+         <ActivityCard 
+          img={activities01}
+          title="Horseback riding"
+          text="This card has supporting text below as a natural lead-in to additional
+        content."
+         />
       </CardDeck>
 
       <CardDeck className="mt-5">
-        <Card>
-          <Card.Img variant="top" src={activities03} />
-          <Card.Body>
-            <Card.Title>Rafting</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
-      </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={activities04} />
-          <Card.Body>
-            <Card.Title>Camp Firing </Card.Title>
-            <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-            </Card.Text>
-          </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
-        </Card>
+        <ActivityCard
+        img={activities03}
+          title="Rafting"
+          text="This card has supporting text below as a natural lead-in to additional
+        content."
+         />
+         <ActivityCard
+        img={activities04}
+          title="Camp Firing"
+          text="This card has supporting text below as a natural lead-in to additional
+        content."
+         />
       </CardDeck>
     </div>
   );
---
+++ src/components/activities/ActivityCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+const ActivityCard = props => {
+  return (
+    <>
+      <Card>
+        <Card.Img variant="top" src={props.img} />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.text}
+          </Card.Text>
+        </Card.Body>
+        {/* <Button>Check it out</Button> */}
+      </Card>
+    </>
+  )
+}
+
+export default ActivityCard;
