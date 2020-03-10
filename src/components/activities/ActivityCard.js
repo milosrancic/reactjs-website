2c99d387497a54ccb37fdb276d56ac417efeefaf
Milos <milosrancic94@gmail.com>
2020-03-10 16:43:29 +0100

    /

--- src/components/activities/ActivityCard.js
+++ src/components/activities/ActivityCard.js
@@ -5,18 +5,16 @@ import Card from 'react-bootstrap/Card'
 
 const ActivityCard = props => {
   return (
-    <>
-      <Card>
-        <Card.Img variant="top" src={props.img} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Text>
-            {props.text}
-          </Card.Text>
-        </Card.Body>
-        {/* <Button>Check it out</Button> */}
-      </Card>
-    </>
+    <Card>
+      <Card.Img variant="top" src={props.img} />
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text>
+          {props.text}
+        </Card.Text>
+      </Card.Body>
+      {/* <Button>Check it out</Button> */}
+    </Card>
   )
 }
 
