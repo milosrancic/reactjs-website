0412ef535818483a8eab692b9ca655ad609e11c1
Milos <milosrancic94@gmail.com>
2020-03-10 16:22:33 +0100

    /

--- src/components/activities/ActivityCard.js
+++ src/components/activities/ActivityCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 
 const ActivityCard = props => {
   return (
