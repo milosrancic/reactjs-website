4683387dccb559c258a8df50c2f3a30a4085f7e5
Milos <milosrancic94@gmail.com>
2020-03-17 17:04:28 +0100

    deleted comments and added padding to datepicker

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -3,24 +3,6 @@ import "../styles/Bookings.css";
 // import Calendar from "react-calendar";
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
-// const Bookings = () => {
-//   return (
-//     <div id="bookings" className="container">
-//       <div className="row">
-//         <div className="col">
-//           <p>Check-in</p>
-//           <Calendar />
-//         </div>
-
-//         <div className="col">
-//           <p>Check-out</p>
-//           <Calendar />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 class Bookings extends React.Component {
   state = {
     date: [new Date(), new Date()],
@@ -35,13 +17,12 @@ class Bookings extends React.Component {
           onChange={this.onChange}
           value={this.state.date}
           format="dd-MM-y"
-          rangeDivider=" to "
           autoFocus={true}
+          isOpen={true}
         />
       </div>
     );
   }
 }
-// isOpen={true}
 
 export default Bookings;
