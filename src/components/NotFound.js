b41d02e8fe2a79731e7e74a6bfcafe5adfdddebf
Milos <milosrancic94@gmail.com>
2020-03-18 12:54:55 +0100

    updated styling for component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const style = {
-  marginTop: "170px",
-  letterSpacing: "1px",
-  textAlign: "center"
-}
+const NotFound = () => {
+  const style = {
+    marginTop: "170px",
+    letterSpacing: "1px",
+    textAlign: "center"
+  };
 
-const NotFound = () => (
-  <h4 style={style}>
-    Sorry, but the page you have requested was not found.
-  </h4>
-);
+  return (
+    <h4 style={style}>
+      Sorry, but the page you have requested was not found.
+    </h4>
+  )
+};
 
 export default NotFound;
