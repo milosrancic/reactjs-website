d07aeb691a79201711b3d5a7096a61287d4a633d
Milos <milosrancic94@gmail.com>
2020-03-22 16:47:33 +0100

    changed file extension from css to scss

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import "./App.css";
+import "./App.scss";
 
 import Navbar from "../components/Navbar";
 import Home from "../components/home/Home";
