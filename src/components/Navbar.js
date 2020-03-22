9f0b780f5593972ba3ab589ee9d44829f70334b9
Milos <milosrancic94@gmail.com>
2020-03-22 16:47:01 +0100

    changed file extension

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav'
 
-import "../styles/Navbar.css";
+import "../styles/Navbar.scss";
 
 const Navbar = () => {
   return (
