9d3bd4c247fb94778e2159be323404315d7c2169
Milos <milosrancic94@gmail.com>
2020-03-24 11:44:49 +0100

    edited bookings

--- src/components/bookings/Bookings.js
+++ src/components/bookings/Bookings.js
@@ -11,18 +11,27 @@ class Bookings extends Component {
 
   render() {
     return (
-      <div className="Bookings container text-center my-5">
+      <div className="Bookings container  my-5">
         <h2>Book your stay</h2>
-        <DateRangePicker
-          onChange={this.onChange}
-          value={this.state.date}
-          format="dd-MM-y"
-          autoFocus={true}
-        />
+
+        <div className="text-center my-5">
+          <DateRangePicker
+            onChange={this.onChange}
+            value={this.state.date}
+            format="dd-MM-y"
+
+            autoFocus={true}
+          />
+        </div>
+
+        <div className="row">
+          <div className="col-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis ratione culpa mollitia unde tempora iusto, dolorum obcaecati nemo repellat, nulla harum doloribus possimus sequi reiciendis repellendus! Quo ea hic nulla illo nobis iure omnis itaque, labore cum qui, quisquam assumenda. Magni minus id ducimus fugit.</div>
+          <div className="col-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem mollitia rerum soluta voluptatem aliquam voluptates eligendi commodi ex repellendus cumque beatae nesciunt, corrupti est reiciendis eum suscipit fugiat molestiae fuga repellat accusantium ullam necessitatibus ipsum?</div>
+        </div>
+
       </div>
     );
   }
 }
-// isOpen={true}
 
 export default Bookings;
