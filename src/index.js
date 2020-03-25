9f69312d96501d9b5dd4a2712d685ca954c4064a
Mita <Mita@mitas-MacBook-Pro.local>
2020-03-25 15:22:26 +0100

    ;./

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/app/App";
 import { BrowserRouter } from "react-router-dom";
 
