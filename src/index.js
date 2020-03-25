a543d89caaf8ec0193cd6cd9f0824f9cebf80aaf
Mita <Mita@mitas-MacBook-Pro.local>
2020-03-25 15:07:13 +0100

    added basename property to browser router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/app/App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/reactjs-website">
     <App />
   </BrowserRouter>,
   document.getElementById("root")
