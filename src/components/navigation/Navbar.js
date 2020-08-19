f8a4c94cea4d0e6947ccf9c3a2936132c3daf66e
Mita <milosrancic94@gmail.com>
2020-08-19 17:11:41 +0200

    code cleanup

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -8,27 +8,29 @@ const Navbar = () => {
   return (
     <Nav className="Navbar fixed-top justify-content-center" activeKey="/home">
       <Nav.Item>
-        <NavLink exact to={"/"} className="nav-link" activeClassName="active-link">Home</NavLink>
+        <NavLink exact to={"/"} className="nav-link" activeClassName="active-link">
+          Home
+        </NavLink>
       </Nav.Item>
       <Nav.Item>
         <NavLink to={"/accomodation"} className="nav-link" activeClassName="active-link">
           Accomodation
-          </NavLink>
+        </NavLink>
       </Nav.Item>
       <Nav.Item>
         <NavLink to={"/activities"} className="nav-link" activeClassName="active-link">
           Activities
-          </NavLink>
+        </NavLink>
       </Nav.Item>
       <Nav.Item>
         <NavLink to={"/bookings"} className="nav-link" activeClassName="active-link">
           Bookings
-          </NavLink>
+        </NavLink>
       </Nav.Item>
       <Nav.Item>
         <NavLink to={"/contact"} className="nav-link" activeClassName="active-link">
           Contact
-          </NavLink>
+        </NavLink>
       </Nav.Item>
     </Nav>
   );
