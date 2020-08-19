c1964a6389ee1bfa8bf34a4fdc15f0e26ea27f49
Mita <milosrancic94@gmail.com>
2020-08-19 17:19:39 +0200

    Updates

---
+++ precache-manifest.567d23d7c2722eae6caffba800c7ec8f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85450d78ccd76a5df72a2889db1217c",
+    "url": "/reactjs-website/index.html"
+  },
+  {
+    "revision": "3f9423c2bcc1e6728c72",
+    "url": "/reactjs-website/static/css/2.6508e315.chunk.css"
+  },
+  {
+    "revision": "1c9733be188d811172ef",
+    "url": "/reactjs-website/static/css/main.1849bfc4.chunk.css"
+  },
+  {
+    "revision": "3f9423c2bcc1e6728c72",
+    "url": "/reactjs-website/static/js/2.348a7f64.chunk.js"
+  },
+  {
+    "revision": "28ba32788ee2748d4729f0fff38d6677",
+    "url": "/reactjs-website/static/js/2.348a7f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9733be188d811172ef",
+    "url": "/reactjs-website/static/js/main.90af45f0.chunk.js"
+  },
+  {
+    "revision": "f69ffeaff8263306b4c8",
+    "url": "/reactjs-website/static/js/runtime-main.81fd43ae.js"
+  },
+  {
+    "revision": "4a58c7cf133e079a418ac543776d58d0",
+    "url": "/reactjs-website/static/media/accomodation-01.4a58c7cf.jpg"
+  },
+  {
+    "revision": "99fbe7906ff32cf1c359cae1a9103917",
+    "url": "/reactjs-website/static/media/accomodation-02.99fbe790.jpeg"
+  },
+  {
+    "revision": "4e178dc3a2b02e5b63b4ac12ec495bbf",
+    "url": "/reactjs-website/static/media/accomodation-03.4e178dc3.jpg"
+  },
+  {
+    "revision": "56ab448d38426e7558f6f64bd5fef39f",
+    "url": "/reactjs-website/static/media/accomodation-04.56ab448d.jpg"
+  },
+  {
+    "revision": "185da948bb1048c14faa7c91ae45b0bc",
+    "url": "/reactjs-website/static/media/accomodation-05.185da948.jpg"
+  },
+  {
+    "revision": "8193f18868985b0aca4098a76b62dabc",
+    "url": "/reactjs-website/static/media/activities-01.8193f188.jpeg"
+  },
+  {
+    "revision": "10ddf3cd06f8092684f49f8fb41e0410",
+    "url": "/reactjs-website/static/media/activities-02.10ddf3cd.jpeg"
+  },
+  {
+    "revision": "2048898fef192ae2dfd006009b8d961d",
+    "url": "/reactjs-website/static/media/activities-03.2048898f.jpeg"
+  },
+  {
+    "revision": "55dfc5a01b4d3b6b165659ae270e3763",
+    "url": "/reactjs-website/static/media/activities-04.55dfc5a0.jpeg"
+  },
+  {
+    "revision": "07bf0d7963a49f660d5b42b0d44c6672",
+    "url": "/reactjs-website/static/media/activities-05.07bf0d79.jpeg"
+  },
+  {
+    "revision": "80ad05ebb8827a27b34057617a038f9b",
+    "url": "/reactjs-website/static/media/activities-06.80ad05eb.jpeg"
+  },
+  {
+    "revision": "b10ebc3bd62e0b90fbb49a1b9403e400",
+    "url": "/reactjs-website/static/media/carousel-01.b10ebc3b.jpg"
+  },
+  {
+    "revision": "b1ac26955b6678d99b840b0260fafd17",
+    "url": "/reactjs-website/static/media/carousel-02.b1ac2695.jpg"
+  },
+  {
+    "revision": "c28da05f924ed988cadcce68694d0b81",
+    "url": "/reactjs-website/static/media/carousel-03.c28da05f.jpg"
+  },
+  {
+    "revision": "c1650966192a18cbf08a30a870f565a7",
+    "url": "/reactjs-website/static/media/carousel-04.c1650966.jpg"
+  },
+  {
+    "revision": "a8be3cbe24f37a76b28e08ad2addc59b",
+    "url": "/reactjs-website/static/media/carousel-05.a8be3cbe.jpg"
+  }
+]);
