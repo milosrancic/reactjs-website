c97b497783c8142af39740ba6c0953d853a0423a
Mita <milosrancic94@gmail.com>
2020-08-19 14:52:36 +0200

    added alt tag, fixed code indentation

--- src/components/accomodation/Room/Room.js
+++ src/components/accomodation/Room/Room.js
@@ -6,37 +6,32 @@ const Room = props => {
     <div className="Room">
       <div className="row mt-5">
         <div className="col-md-8">
-          <img src={props.img} className="img-fluid" alt="" />
+          <img src={props.img} className="img-fluid" alt="accomodation room" />
         </div>
         <div className="col-md-4">
           <span className="feedback">{props.feedback}</span>
-          <h4>{props.title}</h4>
+          <h4 className="title">{props.title}</h4>
           <p>{props.subtitle}</p>
-          <ul>
+          <ul className="icons">
             <li>
               <i className="fas fa-bed"></i> {props.beds}
             </li>
             <li>
-              <i className="fas fa-male"></i>
-              {props.people}
+              <i className="fas fa-male"></i> {props.people}
             </li>
             <li>
-              <i className="fas fa-glass-martini-alt"></i>
-              {props.bar}
+              <i className="fas fa-glass-martini-alt"></i> {props.bar}
             </li>
             <li>
               <i className="fas fa-utensils"></i> {props.food}
             </li>
             <li>
-              <i className="fas fa-dumbbell"></i>
-              {props.gym}
+              <i className="fas fa-dumbbell"></i> {props.gym}
             </li>
             <li>
               <i className="fas fa-spa"></i> {props.spa}
             </li>
           </ul>
-
-          <p>{props.sentence}</p>
         </div>
       </div>
     </div>
