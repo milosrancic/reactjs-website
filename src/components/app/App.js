3150f4e2a0da675e7be4d8213c9cf6e5aa099ef9
Mita <milosrancic94@gmail.com>
2020-08-19 19:30:10 +0200

    definitely fixed it

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Navbar from "../navigation/Navbar";
 import Home from "../home/Home";
