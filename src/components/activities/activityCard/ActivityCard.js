a40e78a3846b15a753bea3066d21f9cf6d95cea2
Mita <milosrancic94@gmail.com>
2020-11-03 18:13:04 +0100

    changed btn color and letter spacing

--- src/components/activities/activityCard/ActivityCard.js
+++ src/components/activities/activityCard/ActivityCard.js
@@ -1,20 +1,28 @@
 import React from "react";
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
-const ActivityCard = props => {
+const ActivityCard = (props) => {
   return (
     <Card>
       <Card.Img variant="top" src={props.img} alt={props.title} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Lorem ipsum sit dolor lorem sit, amet consectetur adipisicing sit elit. Ratione nem impedit accusantium deserunt, dolor dolorum dolor lorem ipsum.
+          Lorem ipsum sit dolor lorem sit, amet consectetur adipisicing sit
+          elit. Ratione nem impedit accusantium deserunt, dolor dolorum dolor
+          lorem ipsum.
         </Card.Text>
-        <Button className="float-right p-2" variant="outline-secondary" size="sm">Check it out</Button>
+        <Button
+          className="float-right p-1 activity-btn"
+          variant="outline-danger"
+          size="sm"
+        >
+          Check it out
+        </Button>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
 export default ActivityCard;
